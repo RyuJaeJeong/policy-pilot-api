7b8a904299882e009a093118f1b54d64f00fe249
unknown <3240133@dgb.vdi>
2025-04-04 11:37:05 +0900

    feat 스트리밍 회신 구현 2025-04-04

--- app/routers/chat_router.py
+++ app/routers/chat_router.py
@@ -1,15 +1,21 @@
 from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
 from typing import Union
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
 from icecream import ic
 import os
 import uuid
+import asyncio
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/chat",
+    tags=["chat"]
+)
+
 model = init_chat_model("gpt-4o-mini-2024-07-18", model_provider="openai")
 
 """
@@ -30,9 +36,9 @@ prompt_template = ChatPromptTemplate.from_messages(
 )
 # 모델 호출 함수    
 async def call_model(state: MessagesState):
-    prompt = prompt_template.invoke(state)
+    prompt = await prompt_template.ainvoke(state)
     ic(prompt)
-    response = model.invoke(prompt)
+    response = await model.ainvoke(prompt)
     return { "messages" : response}  
 
 # 그래프 구성
@@ -49,8 +55,16 @@ app = workflow.compile(checkpointer=MemorySaver())
    ************************************
 """
 
-@router.get("/chat")
-async def chat(thread_id: Union[str, None] = None, query: Union[str, None] = None):
+"""
+ 완성 채팅 반환 
+ 
+ DX사업팀 류재정 프로
+ 
+ @param thread_id: 채팅방 아이디, query: 질의
+ @return result.code: 상태코드, result.msg: 메세지, result.data: 채팅방아이디, 생성 텍스트
+"""
+@router.get("/completion")
+async def completion(thread_id: Union[str, None] = None, query: Union[str, None] = None):
     if thread_id == None:
         thread_id = str(uuid.uuid4())
     config = {"configurable": {"thread_id" : thread_id}}
@@ -61,5 +75,24 @@ async def chat(thread_id: Union[str, None] = None, query: Union[str, None] = Non
         "msg": "성공입니다.",
         "data": { "thread_id": thread_id, "messages": messages},
     }       
-    return result     
-    
+    return result
+
+async def generate_stream(config, query):
+    async for chunk, metadata in app.astream({"messages": query}, config, stream_mode="messages"):
+        if isinstance(chunk, AIMessage): 
+           yield f"{chunk.content}\n"
+
+"""
+ 스트리밍 채팅 반환 
+ 
+ DX사업팀 류재정 프로
+ 
+ @param thread_id: 채팅방 아이디, query: 질의
+ @return 답변 텍스트 
+"""
+@router.get("/streaming")
+async def streaming(thread_id: Union[str, None] = None, query: Union[str, None] = None):
+    if thread_id == None:
+        thread_id = str(uuid.uuid4())
+    config = {"configurable": {"thread_id" : thread_id}}
+    return StreamingResponse(generate_stream(config, query), media_type="text/plain")     # sse 형식의 경우, text/event-stream
