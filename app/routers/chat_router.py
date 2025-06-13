6920b139e3ea82cbc8c6a54cfdd53ad4745c3e8a
DGB\3240133 <fb0982@naver.com>
2025-06-13 11:00:58 +0900

    style : chat_router 코드 수정 1. 랭퓨즈 파라미터 env파일 추가 2. 리트리버 수정 2025-06-13 류재정

--- app/routers/chat_router.py
+++ app/routers/chat_router.py
@@ -1,62 +1,72 @@
 from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
-from icecream import ic
 from langchain_core.messages import AIMessage
 from langchain_core.runnables import RunnableConfig
+from langfuse.callback import CallbackHandler
 from ..schemas.chat_schema import State, ChatResponse
-from ..services.retriever_service import RetrieverService
+from ..services.contextual_compression_retriever_service import ContextualCompressionRetrieverService
 from typing import Union
 from ..utils.llm_model import get_llm
 import logging
 import uuid
+import os
 
-router = APIRouter(
-    prefix="/chat",
-    tags=["chat"]
+# field
+router = APIRouter(prefix="/chat", tags=["chat"])
+langfuse_host = os.environ["LANGFUSE_HOST"]
+langfuse_public_key = os.environ["LANGFUSE_PUBLIC_KEY"]
+langfuse_secret_key = os.environ["LANGFUSE_SECRET_KEY"]
+langfuse_handler = CallbackHandler(
+    public_key=langfuse_public_key,
+    secret_key=langfuse_secret_key,
+    host=langfuse_host
 )
-
-service = RetrieverService(llm=get_llm(), col_nm="vine_policies_20250605_001")
+service = ContextualCompressionRetrieverService(llm=get_llm(), col_nm="vine_policies_20250605_001")
 app = service.build_workflow()
 
-async def generate_stream(state:State, config:RunnableConfig):
-    async for chunk, metadata in app.astream(state, config, stream_mode="messages"):
-        if isinstance(chunk, AIMessage):
-           yield f"{chunk.content}"
 
-async def error_stream(e):
-    yield f"data: [ERROR] {str(e)}\n\n"
-    yield f"data: [DONE]\n\n"
-
+# functions
 @router.get("/completion")
 async def completion(thread_id: Union[str, None] = None, query: Union[str, None] = None) -> ChatResponse:
     """ 완성형 채팅 반환 """
-    if not query:
-        return ChatResponse(code=400, msg="질문이 없습니다", data=None)
     try:
+        if not query:
+            logging.error("=========== 질문이 없습니다. ===========")
+            return ChatResponse(code=400, msg="질문이 없습니다", data=None)
         if thread_id is None:
             thread_id = str(uuid.uuid4())
         state = State(question=query, context=None, answer=None)
-        config = RunnableConfig(configurable={"thread_id" : thread_id})
+        config = RunnableConfig(configurable={"thread_id": thread_id}, callbacks=[langfuse_handler])
         output = await app.ainvoke(state, config)
-        ic(output)
         return ChatResponse(code=200, msg="성공입니다", data=output)
     except Exception as e:
-        logging.error(f"에러발생 : {e}")
+        logging.error(f"에러 발생 : {e}")
         return ChatResponse(code=500, msg="서버 내부 오류", data=None)
+
 
 @router.get("/streaming")
 async def streaming(thread_id: Union[str, None] = None, query: Union[str, None] = None) -> StreamingResponse:
     """ 스트리밍 채팅 반환 """
     try:
         if not query:
+            logging.error("=========== 질문이 없습니다. ===========")
             raise ValueError("질문이 없습니다")
         if thread_id is None:
             thread_id = str(uuid.uuid4())
         state = State(question=query, context=None, answer=None)
-        config = RunnableConfig(configurable={"thread_id" : thread_id})
+        config = RunnableConfig(configurable={"thread_id": thread_id}, callbacks=[langfuse_handler])
         return StreamingResponse(generate_stream(state, config), media_type="text/event-stream")
     except Exception as e:
-        logging.error(f"에러 입니다 : {e}")
+        logging.error(f"에러 발생 : {e}")
         return StreamingResponse(error_stream(e), media_type="text/event-stream")
 
-    
+
+async def generate_stream(state:State, config:RunnableConfig):
+    async for chunk, metadata in app.astream(state, config, stream_mode="messages"):
+        if metadata['langgraph_node'] == "generate" and isinstance(chunk, AIMessage):
+            yield f"{chunk.content}"
+
+
+async def error_stream(e):
+    yield f"data: [ERROR] {str(e)}\n\n"
+    yield f"data: [DONE]\n\n"
