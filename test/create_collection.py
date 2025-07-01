b86a0753ff28f0ff196a473252368c480025686d
DGB\3240133 <fb0982@naver.com>
2025-07-01 17:57:04 +0900

    test: 데이터 추가 코드 2025-07-01 류재정

--- test/create_collection.py
+++ test/create_collection.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 from qdrant_client import QdrantClient, models
-from langchain_qdrant import QdrantVectorStore, RetrievalMode, FastEmbedSparse
+from qdrant_client.http.models import VectorParams
+from langchain_qdrant import QdrantVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.documents import Document
 from dotenv import load_dotenv
@@ -37,32 +38,20 @@ docs = [Document(page_content=f"""제 {row["장"]} 장\n제 {row["조"]} 조 [{r
 
 """ collection 생성 """
 client = QdrantClient(path="./app/vdb")
-col_nm = "sample_policies_20250612_hybrid_02"
+col_nm = "vine_policies_20250701_10"
 dense_vector_nm = "dense"
-sparse_vector_nm = "sparse"
 if not client.collection_exists(col_nm):
     client.create_collection(
         collection_name=col_nm,
-        vectors_config={
-            dense_vector_nm: models.VectorParams(size=3072, distance=models.Distance.COSINE)
-        },
-        sparse_vectors_config={
-            sparse_vector_nm: models.SparseVectorParams(index=models.SparseIndexParams(on_disk=True))
-        }
+        vectors_config= VectorParams(size=3072, distance=models.Distance.COSINE)
     )
 
 """ vectorstore 생성 """
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-sparse_model_nm = "Qdrant/bm25"
-sparse_embeddings = FastEmbedSparse(model_name=sparse_model_nm, cache_dir="./cache")
 qdrant = QdrantVectorStore(
     client=client,
     collection_name=col_nm,
-    embedding=embeddings,
-    sparse_embedding=sparse_embeddings,
-    retrieval_mode=RetrievalMode.HYBRID,
-    vector_name=dense_vector_nm,
-    sparse_vector_name=sparse_vector_nm
+    embedding=embeddings
 )
 
 """ 데이터 입력 """
